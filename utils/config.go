bf75d4865f45d62024b300c2f8610214b84a1fe1
Younes Bouchbouk <younesbouchbouk.py@gmail.com>
2023-05-02 02:16:18 +0100

    move migrationup to main.go insted of dockerfile

--- utils/config.go
+++ utils/config.go
@@ -10,6 +10,7 @@ type Config struct {
 	DBDriver             string        `mapstructure:"DB_DRIVER"`
 	DBSource             string        `mapstructure:"DB_SOURCE"`
 	ServerAddress        string        `mapstructure:"HTTP_SERVER_ADDRESS"`
+	MigrationUrl         string        `mapstructure:"MIGRATION_URL"`
 	GrpcServerAddress    string        `mapstructure:"GRPC_SERVER_ADDRESS"`
 	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
 	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
